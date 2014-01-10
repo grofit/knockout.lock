604675de2765a8f295f1b17540361cb70d2d9d50
Grofit <grofit1@yahoo.com>
2014-01-10 15:49:07 +0000

    Base check-in

---
+++ readme.md
@@ -0,0 +1,35 @@
+# Knockout.Lock
+
+A simple binding to lock and unlock elements within a container
+
+## More Info
+This is quite useful when you want to disable a form while an ajax action or waiting on a callback.
+
+## Usage
+The binding can be called with an observable boolean:
+```
+<div data-bind="lock: someObservableBoolean">
+ ...
+</div>
+```
+
+Or it can be called with a collection of options if you require more control over the locking:
+```
+<div data-bind="lock: { shouldLock: someObservableBoolean, onLocking someLockingCallback, onUnlocking: someUnlockingCallback }">
+ ...
+</div>
+
+<script type="text/javascript">
+	viewModel.someLockingCallback = function(element) { doSomethingWithElement(element); };
+	viewModel.someUnlockingCallback = function(element) { doSomethingWithElement(element); };
+</script>
+```
+
+The available advanced options for this binding are:
+
+* **shouldLock** - The observable boolean value specifying if things elements should be locked
+* **onLocking** - The callback for each element which will be locked
+* **onUnlocking** - The callback for each element which will be unlocked
+
+Here is an example of what it does and how to use it.
+[View Example](https://rawgithub.com/grofit/knockout.lock/master/example.html)
---
+++ src/knockout.lock.js
@@ -0,0 +1,43 @@
+(function(knockout){
+
+	var lockContainer = function(element, lockingCallback) {
+		var nodes = element.getElementsByTagName('*');
+		for(var i = 0; i < nodes.length; i++)
+		{ 
+			nodes[i].disabled = true;
+			if(lockingCallback) 
+			{ lockingCallback(nodes[i]); }
+		}
+	};
+	
+	var unlockContainer = function(element, unlockingCallback) {
+		var nodes = element.getElementsByTagName('*');
+		for(var i = 0; i < nodes.length; i++)
+		{ 
+			nodes[i].disabled = false;
+			if(unlockingCallback)
+			{ unlockingCallback(nodes[i]); }
+		}
+	};
+		
+	knockout.bindingHandlers.lock = {
+		update: function(element, valueAccessor, allBindingsAccessor, viewModel) {
+			var allBindings = allBindingsAccessor();
+			var lockBinding = allBindings.lock;
+			var lock = ko.unwrap(lockBinding);
+			var lockingCallback, unlockingCallback;
+			
+			if(typeof(lock) === "object") {
+				lockingCallback = ko.unwrap(lock.onLocking);
+				unlockingCallback = ko.unwrap(lock.onUnlocking);
+				lock = ko.unwrap(lock.shouldLock);
+			}
+			
+			if (typeof(lock) === "function")
+			{ lock = ko.unwrap(lockBinding)(); }
+
+			lock ? lockContainer(element, lockingCallback) : unlockContainer(element, unlockingCallback);
+		}
+	};
+	
+})(typeof exports === 'undefined' ? this["ko"] : require("knockout"));
